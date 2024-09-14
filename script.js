9389b1f4856d4468f52a6c8fcbbac1c80e6884e4
gkivan-star <gkirengaivan@gmail.com>
2024-09-14 20:28:35 +0200

    Updated files

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sendIP = async () => {
         try {
-            // Get the IP address using ipify API
+            // Get the IP address
             const ipAddress = await fetch('https://api.ipify.org?format=json')
                 .then(response => response.json())
                 .then(data => data.ip);
 
-            // Backend URL
-            const backendURL = 'http://192.168.1.75:5000'; // Flask server URL
-
-            // Send IP Address to the Flask Server
-            const response = await fetch(`${backendURL}/ip`, {
+            // Send IP Address to Server
+            const response = await fetch('https://f961-2c0f-eb68-626-7600-d8f7-3476-d197-d1d5.ngrok-free.app/ip', { // Use your ngrok URL here
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    ip: ipAddress, // Send the IP address to the Flask server
+                    ipAddress: ipAddress,
                 }),
             });
 
             if (response.ok) {
                 console.log('IP address sent to server successfully.');
-                document.getElementById('status').innerText = 'IP Address Sent!';
-                document.getElementById('sub-status').innerText = 'Thank you!';
             } else {
                 console.error('Failed to send IP address.');
-                document.getElementById('status').innerText = 'Failed to Send IP!';
-                document.getElementById('sub-status').innerText = 'Please try again.';
             }
         } catch (error) {
             console.error('Error sending IP address:', error);
-            document.getElementById('status').innerText = 'Error!';
-            document.getElementById('sub-status').innerText = 'Please try again later.';
         }
     };
 
